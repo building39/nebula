d15283f7e5b8f1e89ef30aa88dcca89175ec60a0
mmartin <mmartin@mezeo.com>
2016-10-04 13:03:11 -0500

    Add new module defining system-wide constants

---
+++ web/util/constants.ex
@@ -0,0 +1,10 @@
+defmodule Nebula.Util.Constants do
+  @moduledoc """
+  Define system-wide constants here.
+  """
+
+  defmacro system_domain_uri do
+    "/cdmi_domains/system_domain/"
+  end
+
+end
