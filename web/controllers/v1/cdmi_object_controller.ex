afeefc249ccb2950108327c679d720f3095fa176
mmartin <mmartin4242@gmail.com>
2016-10-07 13:56:38 -0500

    Stub out actions with 501 Not Implemented.:q

--- web/controllers/v1/cdmi_object_controller.ex
+++ web/controllers/v1/cdmi_object_controller.ex
@@ -9,24 +9,15 @@ defmodule Nebula.V1.CdmiObjectController do
   require Logger
 
   def index(conn, _params) do
-#    cdmi_objects = Repo.all(CdmiObject)
-#    render(conn, "index.json", cdmi_objects: cdmi_objects)
+    conn
+    |> put_status(501)
+    |> json(%{error: "Not Implemented"})
   end
 
   def create(conn, %{"cdmi_object" => cdmi_object_params}) do
-#    changeset = CdmiObject.changeset(%CdmiObject{}, cdmi_object_params)
-
-#    case Repo.insert(changeset) do
-#      {:ok, cdmi_object} ->
-#        conn
-#        |> put_status(:created)
-#        |> put_resp_header("location", cdmi_object_path(conn, :show, cdmi_object))
-#        |> render("show.json", cdmi_object: cdmi_object)
-#      {:error, changeset} ->
-#        conn
-#        |> put_status(:unprocessable_entity)
-#        |> render(Nebula.ChangesetView, "error.json", changeset: changeset)
-#    end
+    conn
+    |> put_status(501)
+    |> json(%{error: "Not Implemented"})
   end
 
   def show(conn, %{"id" => id}) do
@@ -39,27 +30,15 @@ defmodule Nebula.V1.CdmiObjectController do
   end
 
   def update(conn, %{"id" => id, "cdmi_object" => cdmi_object_params}) do
-#    cdmi_object = Repo.get!(CdmiObject, id)
-#    changeset = CdmiObject.changeset(cdmi_object, cdmi_object_params)
-
-#    case Repo.update(changeset) do
-#      {:ok, cdmi_object} ->
-#        render(conn, "show.json", cdmi_object: cdmi_object)
-#      {:error, changeset} ->
-#        conn
-#        |> put_status(:unprocessable_entity)
-#        |> render(Nebula.ChangesetView, "error.json", changeset: changeset)
-#    end
+    conn
+    |> put_status(501)
+    |> json(%{error: "Not Implemented"})
   end
 
   def delete(conn, %{"id" => id}) do
-#    cdmi_object = Repo.get!(CdmiObject, id)
-#
-#    # Here we use delete! (with a bang) because we expect
-#    # it to always work (and if it does not, it will raise).
-#    Repo.delete!(cdmi_object)
-#
-#    send_resp(conn, :no_content, "")
+    conn
+    |> put_status(501)
+    |> json(%{error: "Not Implemented"})
   end
 
 end
--- web/controllers/v1/container_controller.ex
+++ web/controllers/v1/container_controller.ex
@@ -1,29 +1,29 @@
 defmodule Nebula.V1.ContainerController do
-  use Nebula.Web, :controller
+  @moduledoc """
+  Handle cdmi containers
+  """
 
+  use Nebula.Web, :controller
+  import Nebula.Util.Constants, only: :macros
   alias Nebula.Container
   require Logger
 
+  @doc """
+  When a get for the root container is requested, this is where
+  we end up.
+  """
   def index(conn, _params) do
-#    containers = Repo.all(Container)
-#    render(conn, "index.json", containers: containers)
+    Logger.debug("Entry to Controller.index")
+    conn
+    |> put_status(501)
+    |> json(%{error: "Not Implemented"})
   end
 
   def create(conn, %{"container" => container_params}) do
-#    changeset = Container.changeset(%Container{}, container_params)
-
-#    case Repo.insert(changeset) do
-#      {:ok, container} ->
-#        conn
-#        |> put_status(:created)
-#        |> put_resp_header("location", container_path(conn, :show, container))
-#        |> render("show.json", container: container)
-#      {:error, changeset} ->
-#        conn
-#        |> put_status(:unprocessable_entity)
-#        |> render(Nebula.ChangesetView, "error.json", changeset: changeset)
-#    end
-  end
+    Logger.debug("Entry to Controller.create")
+    conn
+    |> put_status(501)
+    |> json(%{error: "Not Implemented"})  end
 
   def show(conn, %{"id" => id}) do
     Logger.debug("In container show")
@@ -34,26 +34,16 @@ defmodule Nebula.V1.ContainerController do
   end
 
   def update(conn, %{"id" => id, "container" => container_params}) do
-#    container = Repo.get!(Container, id)
-#    changeset = Container.changeset(container, container_params)
-
-#    case Repo.update(changeset) do
-#      {:ok, container} ->
-#        render(conn, "show.json", container: container)
-#      {:error, changeset} ->
-#        conn
-#        |> put_status(:unprocessable_entity)
-#        |> render(Nebula.ChangesetView, "error.json", changeset: changeset)
-#    end
+    Logger.debug("Entry to Controller.update")
+    conn
+    |> put_status(501)
+    |> json(%{error: "Not Implemented"})
   end
 
   def delete(conn, %{"id" => id}) do
-#    container = Repo.get!(Container, id)
-#
-#    # Here we use delete! (with a bang) because we expect
-#    # it to always work (and if it does not, it will raise).
-#    Repo.delete!(container)
-#
-#    send_resp(conn, :no_content, "")
+    Logger.debug("Entry to Controller.delete")
+    conn
+    |> put_status(501)
+    |> json(%{error: "Not Implemented"})
   end
 end
