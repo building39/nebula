3ad762ce678b14f6f1ee0c8f2b08a539fa115871
mmartin <mmartin4242@gmail.com>
2016-10-10 14:34:30 -0500

    Move constants to lib/

---
+++ lib/nebula/constants.ex
@@ -0,0 +1,30 @@
+defmodule Nebula.Constants do
+  @moduledoc """
+  Define system-wide constants here.
+  """
+
+  defmacro api_prefix do
+    "/api/v1/"
+  end
+  
+  defmacro capabilities_object do
+    "application/cdmi-capapbilities"
+  end
+
+  defmacro container_object do
+    "application/cdmi-container"
+  end
+
+  defmacro dataobject_object do
+    "application/cdmi-dataobject"
+  end
+
+  defmacro domain_object do
+    "application/cdmi-domain"
+  end
+
+  defmacro system_domain_uri do
+    "/cdmi_domains/system_domain/"
+  end
+
+end
