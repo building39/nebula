b249192621bfdec3d4654c05b7e51f4b420e9a9b
mmartin <mmartin4242@gmail.com>
2016-10-20 13:25:53 -0500

    Fix cdmi_objectid route

--- web/router.ex
+++ web/router.ex
@@ -5,16 +5,16 @@ defmodule Nebula.Router do
     plug :accepts, ["json"]
     plug Nebula.V1.CDMIVersion
     plug Nebula.V1.Authentication
-    plug Nebula.V1.Prefetch
-    plug Nebula.V1.CheckDomain
-    plug Nebula.V1.ApplyCapabilities
-    plug Nebula.V1.ApplyACLs
+    #plug Nebula.V1.Prefetch
+    #plug Nebula.V1.CheckDomain
+    #plug Nebula.V1.ApplyCapabilities
+    #plug Nebula.V1.ApplyACLs
   end
 
   scope "/api", Nebula do
     pipe_through :api
     scope "/v1", V1, as: :v1 do
-      get "/cdmi_object/:id", CdmiObjectController, :show
+      get "/cdmi_objectid/:id", CdmiObjectController, :show
       get "/container", ContainerController, :show
       get "/container/*path", ContainerController, :show
       put "/container/:path", ContainerController, :create
