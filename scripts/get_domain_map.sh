fdc9e3f57c51b3be70427205c3768167866675ce
mmartin <mmartin4242@gmail.com>
2018-02-02 09:00:03 -0500

    Change port to 4000

--- scripts/get_domain_map.sh
+++ scripts/get_domain_map.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -uadministrator:test -H "x-cdmi-specification-version: 1.1"  -H "Accept: application/cdmi-domain" -X GET "http://localhost:8080/cdmi/system_configuration/domain_maps" -v |python -m json.tool
+curl -uadministrator:test -H "x-cdmi-specification-version: 1.1"  -H "Accept: application/cdmi-domain" -X GET "http://localhost:4000//api/v1/system_configuration/domain_maps" -v |python -m json.tool
