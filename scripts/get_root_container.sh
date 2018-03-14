46f2959ea49aa318b5f05613e9b5f6c9f3b0bf12
mmartin <mmartin4242@gmail.com>
2018-03-14 17:40:19 -0400

    fix url

--- scripts/get_root_container.sh
+++ scripts/get_root_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -uadministrator:test -H "x-cdmi-specification-version: 1.1.1" -H "content-type: application/cdmi-container" -H "Accept: application/cdmi-container" -X GET "http://localhost:4000/api/v1/" -v |python -m json.tool
+curl -uadministrator:test -H "x-cdmi-specification-version: 1.1.1" -H "content-type: application/cdmi-container" -H "Accept: application/cdmi-container" -X GET "http://localhost:4000/cdmi/v1/" -v |python -m json.tool
