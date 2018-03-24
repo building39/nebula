0059936324fa87f985f91fc16a88b6d56772ea03
mmartin <mmartin4242@gmail.com>
2018-03-24 11:09:47 -0400

    Script update

--- scripts/get_domain.sh
+++ scripts/get_domain.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -uadministrator:test -H "x-cdmi-specification-version: 1.1"  -H "Accept: application/cdmi-domain" -X GET "http://localhost:4000/cdmi/v1/cdmi_domains/Fuzzcat5/" -v |python -m json.tool
+curl -uadministrator:test -H "x-cdmi-specification-version: 1.1"  -H "Accept: application/cdmi-domain" -X GET "http://localhost:4000/cdmi/v1/cdmi_domains/Fuzzcat6/" -v |python -m json.tool
