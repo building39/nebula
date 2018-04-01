0e192844a990c4c6416c177a1f40ef93f9dbcf3d
mmartin <mmartin4242@gmail.com>
2018-04-01 13:28:51 -0400

    bug fix

--- scripts/get_dataobject.sh
+++ scripts/get_dataobject.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -u"administrator:test;realm=system_domain" -H "x-cdmi-specification-version: 1.1" -H "Accept: application/cdmi-object" -X GET "http://cloud.fuzzcat.net:8080/cdmi/new_container7/Janice-SchoolPhoto.jpg" -v > /tmp/janice # |python -m json.tool
+curl -u"mmartin:test;realm=default_domain" -H "x-cdmi-specification-version: 1.1" -H "Accept: application/cdmi-object" -X GET "http://localhost:4000/cdmi/v1/new_container3/new_object1.txt" -v > /tmp/janice # |python -m json.tool
 #curl -uadministrator:test -H "x-cdmi-specification-version: 1.1" -H "Accept: application/cdmi-object" -X GET "http://localhost:8080/cdmi/new_container7/multipart6.txt" -v |python -m json.tool
 #curl -v -uadministrator -H "x-cdmi-specification-version: 1.1" -H "content-type: application/cdmi-container" -H "Accept: application/cdmi-container" -X GET "http://localhost:8080/cdmi/system_configuration/?children"
