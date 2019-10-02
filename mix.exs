605560f09934ee05e03744e1455315ca8d996214
mmartin <mmartin4242@gmail.com>
2019-10-02 18:46:13 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nebula.Mixfile do
   def project do
     [
       app: :nebula,
-      version: "0.0.2",
+      version: "0.1.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
