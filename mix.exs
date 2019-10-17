6b21d6e5ad438c1764032f081895997d4ffa1521
mmartin <mmartin4242@gmail.com>
2019-10-17 15:52:34 -0400

    Pare down the dependencies

--- mix.exs
+++ mix.exs
@@ -43,7 +43,6 @@ defmodule Nebula.Mixfile do
         :comeonin,
         :cdmioid,
         :memcache_client,
-        :memcachir,
         :nebula_metadata
       ]
     ]
@@ -59,23 +58,17 @@ defmodule Nebula.Mixfile do
   defp deps do
     [
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
-      {:linguist, "~> 0.1"},
-      {:pooler, "~> 1.5"},
       {:phoenix, "~> 1.4"},
       {:poison, "~> 4.0", override: true},
       {:jason, "~> 1.0"},
-      {:phoenix_pubsub, "~> 1.1"},
       {:gettext, "~> 0.17"},
       {:cowboy, "~> 2.6"},
       {:plug_cowboy, "~> 2.1"},
-      {:plug, "~> 1.8"},
       {:logger_file_backend, "~> 0.0"},
       {:comeonin, "~> 5.0"},
       {:uuid, "~> 1.1"},
       {:hexate, "~> 0.6"},
-      {:memcachir, "~> 3.2"},
       {:memcache_client, "~> 1.1"},
-      {:telemetry, "~> 0.4"},
       {:cdmioid, git: "https://github.com/building39/cdmioid.git", branch: "master"},
       {:mock, "~> 0.3", only: :test},
       {:excoveralls, "~> 0.8", only: :test},
