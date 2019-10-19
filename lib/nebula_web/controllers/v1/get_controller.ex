f7e5b5fbb21f2ed172422d4bd3473e7013f55629
mmartin <mmartin4242@gmail.com>
2019-10-19 11:18:57 -0400

    Fix missing render

--- lib/nebula_web/controllers/v1/get_controller.ex
+++ lib/nebula_web/controllers/v1/get_controller.ex
@@ -21,16 +21,22 @@ defmodule NebulaWeb.V1.GetController do
     Logger.debug(fn -> "conn: #{inspect(conn, pretty: true)}" end)
 
     objectType = conn.assigns.data.objectType
+    Logger.debug("objectType: #{inspect objectType}")
     {_, render_type} = List.keyfind(render_object_type(), objectType, 0)
 
     Logger.debug("render_type: #{inspect(render_type)}")
     data = process_query_string(conn, conn.assigns.data)
     Logger.debug("Returning data: #{inspect(data, pretty: true)}")
 
-    conn
-      |> set_mandatory_response_headers(objectType)
-      |> check_acls()
-      |> put_status(:ok)
-
+    new_conn = conn
+    |> set_mandatory_response_headers(objectType)
+    |> check_acls()
+    |> put_status(:ok)
+    Logger.debug("About to render")
+    r = render(new_conn, render_type, cdmi_object: data)
+    Logger.debug("Render returned #{inspect(r, pretty: true)}")
+    Logger.debug("New conn: #{inspect(new_conn, pretty: true)}")
+    Logger.debug("Leaving get_controller. Conn: #{inspect(new_conn, prettuy: true)}")
+    new_conn
   end
 end
