4c7157d5a9e2a850b709d108ab9eab1e9b6a193f
mmartin <mmartin4242@gmail.com>
2019-10-22 16:41:04 -0400

    Remove debug logging

--- lib/nebula_web/plugs/v1/apply_capabilities.ex
+++ lib/nebula_web/plugs/v1/apply_capabilities.ex
@@ -17,26 +17,22 @@ defmodule Nebula.V1.ApplyCapabilities do
   @doc """
   Document this function
   """
+  def call(conn = %{sys_capabilitiies: _s}) do
+    Logger.debug("already got capabilities")
+    conn
+  end
   def call(conn, _opts) do
     Logger.debug("ApplyCapabilities plug")
     domain_hash = get_domain_hash(system_domain_uri())
     query = "sp:" <> domain_hash <> system_capabilities_uri()
-    Logger.debug("Apply capabilities is Searching...")
     {rc, capabilities} = GenServer.call(Metadata, {:search, query})
-    Logger.debug("Apply capabilities found rc: #{inspect(rc)}")
-    Logger.debug("Apply capabilities found data: #{inspect(capabilities)}")
 
     case rc do
       :ok ->
-        Logger.debug("Capabilities: #{inspect(capabilities)}")
         assign(conn, :sys_capabilities, capabilities)
-        Logger.debug("Conn: #{inspect(conn, pretty: true)}")
-        assign_map = conn.assigns
-        assign_map = Map.put_new(assign_map, :sys_capabilities, capabilities)
-        Map.put(conn, :assigns, assign_map)
 
       _ ->
-        Logger.debug("NotFound")
+        Logger.debug("Capabilities NotFound")
         request_fail(conn, :not_found, "Not found 1")
     end
   end
